7b73b1d661872b7d571a152b75fee81c81bdbb90
Anush Chakhoyan <anush.chakhoyan@develandoo.am>
2021-03-07 00:57:57 +0400

    added exploring link and changed arrows

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import {Container} from "react-bootstrap";
 
-import ProductsListHeader from "../../components/Products/Header";
 import Products from "../../components/Products";
 import WhoWeAre from "../../components/WhoWeAre";
 import Hero from "../../components/Hero";
@@ -9,13 +7,9 @@ import Hero from "../../components/Hero";
 const Home = () => (
     <>
         <Hero/>
-        <section className="bg-white py-5">
-            <Container>
-                <ProductsListHeader title="Recently viewed & more" to="/view-all"/>
-                <Products />
-            </Container>
-        </section>
+        <Products />
         <WhoWeAre />
+        <Products />
     </>
 )
 
