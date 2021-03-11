f0d067f1907b23280fe14966395f5b576a5b29df
Anush Chakhoyan <anush.chakhoyan@develandoo.am>
2021-03-12 00:40:40 +0400

    updated header styles

---
+++ src/components/GlobalSearch/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {FiSearch} from "react-icons/all";
+import {InputGroup} from "react-bootstrap";
+
+import "./index.scss";
+const { Prepend } = InputGroup;
+
+const GlobalSearch = () => (
+    <InputGroup className="ml-5 mr-3 search-wrapper">
+        <Prepend className="cursor-pointer align-items-center">
+            <FiSearch className="search-icon text-white"/>
+        </Prepend>
+        <input type="text" className="custom-input" placeholder="Search"/>
+    </InputGroup>
+)
+
+export default GlobalSearch;
