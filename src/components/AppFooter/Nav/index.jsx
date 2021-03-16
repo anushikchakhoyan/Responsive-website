b7d78ba9ac637805542e9616ecea83c3a3b9a407
Anush Chakhoyan <anush.chakhoyan@develandoo.am>
2021-03-17 01:56:34 +0400

    fixed header padding, removed product item margins, fixed favorites layout title, updated footer nav menu

--- src/components/AppFooter/Nav/index.jsx
+++ src/components/AppFooter/Nav/index.jsx
@@ -1,27 +1,28 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const AppFooterNav = () => (
     <div className="d-flex flex-wrap justify-content-between footer-nav-list">
         <ul className="footer-list p-0">
             <p>Services</p>
-            <li>Services</li>
-            <li>Services</li>
-            <li>Services</li>
-            <li>Services</li>
+            <li><Link to="/">Services</Link></li>
+            <li><Link to="/">Services</Link></li>
+            <li><Link to="/">Services</Link></li>
+            <li><Link to="/">Services</Link></li>
         </ul>
         <ul className="footer-list p-0">
             <p>Software</p>
-            <li>Software</li>
-            <li>Software</li>
-            <li>Software</li>
-            <li>Software</li>
+            <li><Link to="/">Software</Link></li>
+            <li><Link to="/">Software</Link></li>
+            <li><Link to="/">Software</Link></li>
+            <li><Link to="/">Software</Link></li>
         </ul>
         <ul className="footer-list p-0">
             <p>Contact</p>
-            <li>Contact</li>
-            <li>Contact</li>
-            <li>Contact</li>
-            <li>Contact</li>
+            <li><Link to="/">Contact</Link></li>
+            <li><Link to="/">Contact</Link></li>
+            <li><Link to="/">Contact</Link></li>
+            <li><Link to="/">Contact</Link></li>
         </ul>
     </div>
 )
